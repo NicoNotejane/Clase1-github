48bed75bdcece67cd60ca6ce258784553c6aef68
Nicolas Notejane <niconotejane@gmail.com>
2018-12-11 17:11:22 -0300

    Proyecto creado

---
+++ src/aplication/Program.java
@@ -0,0 +1,52 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product p = new Product();
+		
+		System.out.println("Enter the product data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		Product product  = new Product(name, price);
+		
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+	    product.setPrice(1200.00);
+	    System.out.println("Updated price: " + product.getPrice());
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of the products to be added in stock: ");
+	    int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of the products to be removed in stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		sc.close();
+		
+	}
+
+}
